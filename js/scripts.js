e9348023e76edec732ba1515563a13214874016b
Sonam-22 <sonampriya7070@gmail.com>
2022-01-31 01:16:04 +0100

    added IIFE to make pokemonList variable local

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,60 @@
-let pokemonList = [{
-    name: 'Bulbasaur',
-    height: 0.7,
-    type: ['grass', 'poison']
-  },
-  {
-    name: 'Ivysaur',
-    height: 1,
-    type: ['grass', 'poison']
-  },
-  {
-    name: 'Venusaur',
-    height: 2,
-    type: ['grass', 'poison']
-  },
-  {
-    name: 'Charmander ',
-    height: 0.6,
-    type: ['fire']
-  },
-  {
-    name: 'Charmeleon',
-    height: 1.1,
-    type: ['fire']
-  },
-  {
-    name: 'Charizard',
-    height: 1.7,
-    type: ['fire', 'flying']
-  },
-  {
+// IIFE
+let pokemonRepository = (function() {
+  let pokemonList = [{
+      name: 'Bulbasaur',
+      height: 0.7,
+      type: ['grass', 'poison']
+    },
+    {
+      name: 'Ivysaur',
+      height: 1,
+      type: ['grass', 'poison']
+    },
+    {
+      name: 'Venusaur',
+      height: 2,
+      type: ['grass', 'poison']
+    },
+    {
+      name: 'Charmander ',
+      height: 0.6,
+      type: ['fire']
+    },
+    {
+      name: 'Charmeleon',
+      height: 1.1,
+      type: ['fire']
+    },
+    {
+      name: 'Charizard',
+      height: 1.7,
+      type: ['fire', 'flying']
+    },
+    {
 
-    name: 'Squirtle',
-    height: 0.5,
-    type: ['water']
+      name: 'Squirtle',
+      height: 0.5,
+      type: ['water']
+    }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-]
+
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+console.log(pokemonRepository.getAll());
 
 document.write("<div class='pokemon-container'>");
-// forEach() 
-pokemonList.forEach(function(pokemon) {
+// forEach()
+pokemonRepository.getAll().forEach(function(pokemon) {
   //This handles heights greater than or equal to 2
   if (pokemon.height >= 2) {
     document.write(` ${pokemon.name} (height: ${pokemon.height}) - Wow, that’s big!<br> `);

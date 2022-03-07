5fb5bd0b11e0efc4756913b8473ecd035c52df20
Sonam-22 <sonampriya7070@gmail.com>
2022-03-08 00:21:21 +0100

    Added config files for linters. Minified CSS & JS Files.

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,11 @@ let pokemonRepository = (function() {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=50';
 
-
   function add(pokemon) {
-    if (
-      typeof pokemon === "object" &&
-      "name" in pokemon
-    ) {
+    if (typeof pokemon === 'object' && 'name' in pokemon) {
       pokemonList.push(pokemon);
     } else {
-      console.log("pokemon is not correct");
+      console.log('pokemon is not correct');
     }
   }
   // function that allows to find specific Pokémon only by name
@@ -21,7 +17,9 @@ let pokemonRepository = (function() {
       return pokemonList;
     }
     // Return the  pokemons whose name contain the searc term.
-    return pokemonList.filter(pokemon => pokemon.name.toLowerCase().includes(searchName.toLowerCase()));
+    return pokemonList.filter(pokemon =>
+      pokemon.name.toLowerCase().includes(searchName.toLowerCase())
+    );
   }
 
   function loadFilteredPokemonList() {
@@ -30,16 +28,14 @@ let pokemonRepository = (function() {
     // Get reference of pokemon list.
     let pokemonListContainer = $('#pokemon-list');
     // Add event  listener to the search input so that on each letter typed, list gets filtered.
-    let searchInput = $('#search-input')
-      .on('input', event => {
-        // Everytime the user types anything, we should first clear the  existing list.
-        pokemonListContainer.empty();
-        // The we should get the new list filtered by the input provided by user.
-        let filteredList = filteredPokemon(event.target.value);
-        // display each item of the fitered list.
-        filteredList.forEach(addListItem);
-      });
-
+    $('#search-input').on('input', event => {
+      // Everytime the user types anything, we should first clear the  existing list.
+      pokemonListContainer.empty();
+      // The we should get the new list filtered by the input provided by user.
+      let filteredList = filteredPokemon(event.target.value);
+      // display each item of the fitered list.
+      filteredList.forEach(addListItem);
+    });
   }
 
   function getAll() {
@@ -47,15 +43,16 @@ let pokemonRepository = (function() {
   }
   // created function to add, within the pokemon-list ul, list items with buttons holding a Pokemon's name as its inner text.
   function addListItem(pokemon) {
-
     let pokemonList = $('#pokemon-list');
     let pokemonListItembutton = $('<button></button>')
-      .addClass('list-group-item  text-capitalize list-group-item-action text-center')
+      .addClass(
+        'list-group-item  text-capitalize list-group-item-action text-center'
+      )
       .addClass('shadow p-3 mb-5  rounded pokemon-list-item')
-      .attr("data-toggle", "modal")
-      .attr("role", "listitem")
-      .attr("type", "button")
-      .attr("data-target", "#pokemonModal")
+      .attr('data-toggle', 'modal')
+      .attr('role', 'listitem')
+      .attr('type', 'button')
+      .attr('data-target', '#pokemonModal')
       .text(pokemon.name)
       .on({
         click: () => {
@@ -67,42 +64,40 @@ let pokemonRepository = (function() {
 
   // function to load list of pokemon from apiUrl, stores name & detailsUrl in pokemonList via add()
   function loadList() {
-
-    return fetch(apiUrl).then(function(response) {
-
-      return response.json();
-    }).then(function(json) {
-      json.results.forEach(function(item) {
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
+    return fetch(apiUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        json.results.forEach(function(item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function(e) {
+        console.error(e);
       });
-    }).catch(function(e) {
-
-      console.error(e);
-    })
   }
   // function to load further details about pokemon (items) in the pokemonList: image, height & types
   function loadDetails(item) {
-
     let url = item.detailsUrl;
-    return fetch(url).then(function(response) {
-
-      return response.json();
-    }).then(function(details) {
-
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.types = details.types;
-      item.weight = details.weight;
-      item.abilities = details.abilities;
-    }).catch(function(e) {
-
-      console.error(e);
-    });
-
+    return fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(details) {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+        item.weight = details.weight;
+        item.abilities = details.abilities;
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
   }
   // function to show details of the pokemon on the button 'click' event, called above within addListItem function
   function showDetails(item) {
@@ -122,15 +117,24 @@ let pokemonRepository = (function() {
 
     let pokemonHeight = $('<p>' + 'Height: ' + pokemon.height + '</p>');
 
-    let pokemonTypes = $('<p>' + 'Types: ' + pokemon.types.map(item => item.type.name).join(", ") + '</p>');
+    let pokemonTypes = $(
+      '<p>' +
+        'Types: ' +
+        pokemon.types.map(item => item.type.name).join(', ') +
+        '</p>'
+    );
 
     let pokemonWeight = $('<p>' + 'Weight: ' + pokemon.weight + '</p>');
 
-    let pokemonAbilities = $('<p>' + 'Abilities: ' + pokemon.abilities.map(item => item.ability.name).join(", ") + '</p>');
+    let pokemonAbilities = $(
+      '<p>' +
+        'Abilities: ' +
+        pokemon.abilities.map(item => item.ability.name).join(', ') +
+        '</p>'
+    );
 
     let pokemonImage = $('<img class=\'pokemon-modal-image\'>');
     pokemonImage.attr('src', pokemon.imageUrl);
-
 
     modalTitle.append(pokemonName); // pokemonName is displayed as the title in the modal
     modalBody.append(pokemonImage); // pokemonImage is displayed in the body of the modal
@@ -138,7 +142,6 @@ let pokemonRepository = (function() {
     modalBody.append(pokemonWeight); // pokemonWeight is displayed in the body of the modal
     modalBody.append(pokemonAbilities); // pokemonDetails are displayed in the body of the modal
     modalBody.append(pokemonTypes);
-
   }
 
   return {
@@ -152,10 +155,6 @@ let pokemonRepository = (function() {
     showDetails: showDetails
   };
 })();
-console.log(pokemonRepository.getAll());
-
-console.log(pokemonRepository.filteredPokemon('Squirtle'));
-
 pokemonRepository.loadList().then(function() {
   pokemonRepository.loadFilteredPokemonList();
 });
